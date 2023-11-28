3af82177a0fc97227942ba1d28d23b4fd71d32c9
Rustam <rustam17.sb@gmail.com>
2023-11-28 22:59:58 +0300

    feat: add a function to check the ability to use local storage

--- source/js/util.js
+++ source/js/util.js
@@ -4,3 +4,15 @@ export const isUpArrowEvent = (evt) => evt.code === KeyCode.UP_ARROW;
 export const isRightArrowEvent = (evt) => evt.code === KeyCode.RIGHT_ARROW;
 export const isDownArrowEvent = (evt) => evt.code === KeyCode.DOWN_ARROW;
 export const isEscapeEvent = (evt) => evt.code === KeyCode.ESCAPE;
+
+export const checkLocalStorageSupport = () => {
+  let isSupport = true;
+
+  try {
+    localStorage.getItem('test');
+  } catch (err) {
+    isSupport = false;
+  }
+
+  return isSupport;
+};
